50ec6b4e013b8a33fe08164f78237740dc22228c
amefodev <mrmefodij@gmail.com>
2022-06-16 12:14:37 +0300

    First Commit

---
+++ Two_Sum/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+#include "test_runner.h"
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+
+    }
+};
+
+void Examples(){
+
+}
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr,Examples);
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
